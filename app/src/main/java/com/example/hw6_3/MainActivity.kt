70cada2870cb316f51f16cb6b635a9081fe8aed0
razie rezaee <rezaie127@yahoo.com>
2022-02-17 13:46:32 +0330

    add string

--- app/src/main/java/com/example/hw6_3/MainActivity.kt
+++ app/src/main/java/com/example/hw6_3/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var textView=findViewById<TextView>(R.id.textView3)
-        textView.text="Aladdin is a 2019 American musical fantasy drama film produced by Walt Disney Pictures. Directed by Guy Ritchie, from a script he co-wrote with John August, it is a live-action/CGI adaptation of Disney's 1992 animated film of the same name, which itself is based on the eponymous tale from One Thousand and One Nights.[1][a] The film stars Will Smith, Mena Massoud, Naomi Scott, Marwan Kenzari, Navid Negahban, Nasim Pedrad, Billy Magnussen, and Numan Acar, as well as"
+        textView.text=getString(R.string.text)
     }
 }
